091e7855c4b79884c940d7e74dfe3b49b8db6793
Johnsuuuu <46539757+Johnsuuuu@users.noreply.github.com>
2023-05-31 11:00:30 -0700

    modified homepage text

--- client/src/components/studyPages/HomePage.js
+++ client/src/components/studyPages/HomePage.js
@@ -5,14 +5,12 @@ function HomePage() {
     return (
         <div>
             <NavDrawer />
-            <h1 style={{ textAlign: 'center'}}>Study Details</h1>
+            <h1 style={{ textAlign: 'center' }}>Study Details</h1>
             <div style={{ width: "50%", margin: '0 auto' }}>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra ex eget lorem iaculis dapibus. Duis sed dictum sem. Nullam viverra pellentesque tortor id facilisis. Morbi aliquam sed risus vel posuere. Nam aliquet est vel lacus ullamcorper, quis tincidunt lectus efficitur. Vivamus eu nunc vitae turpis mattis interdum. Nulla ut diam sed velit ultricies molestie et in augue. Sed laoreet fermentum est, at gravida nisi vehicula quis. Vivamus vestibulum nibh ac velit lobortis ullamcorper. Aliquam libero nisl, viverra id magna ac, molestie porttitor diam. Sed orci mauris, maximus eget interdum id, ultrices facilisis nunc. Morbi varius congue nunc sit amet auctor. Ut et mi dignissim, bibendum urna ultrices, condimentum nisl.
+                    The primary objective of this study is to explore and understand the perceived realism of two distinct touch signals. Participants will engage in a series of trials, each requiring them to compare and discern which touch signal they perceive as more realistic. In each trial, you will be asked to experience 2 signals and make a decision. There is no right or wrong answer, it purely relies on your personal perception.
                 </p>
-                <p>
-                    Praesent congue sollicitudin tellus, fermentum rhoncus dui rutrum nec. Sed nec mauris porta, pretium augue ac, porttitor velit. Aliquam eu ipsum at lectus porttitor vulputate. Nam quis metus in sem tempor rutrum. Phasellus semper laoreet urna, vitae ullamcorper ipsum bibendum ac. Maecenas convallis, lorem vitae egestas pulvinar, ligula dui maximus sapien, sit amet tristique nisl elit vitae felis. Nullam eget volutpat nunc. Pellentesque tincidunt lorem vitae lectus suscipit, nec rutrum neque ultricies. Proin euismod vel massa eu dictum. Pellentesque in tempor arcu, non lobortis quam. Vestibulum rhoncus iaculis sollicitudin.
-                </p>
+
 
             </div>
 
